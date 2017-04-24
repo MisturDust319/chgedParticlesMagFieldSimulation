29f4ee5015adcc9ae2a1fc05a87ec0e22a8773bb
Stan Slupecki <jarlofearls@gmail.com>
2017-04-24 18:50:58 -0400

    Started adding physics object classes

---
+++ chgedParticlesMagFieldSimulation/js/particle.js
@@ -0,0 +1,11 @@
+﻿Particle.prototype = {
+    mass: 0,
+    position: THREE.Vector3(0, 0, 0),
+    velocity: THREE.Vector3(0, 0, 0),
+    acceleration: THREE.Vector3(0, 0, 0),
+    chgPosition: function () {
+        this.position.add(
+
+        )
+    }
+}
